a347e3240c247b4c8d7f404d92ea2a15dc4764e5
prateekv <raj08prateek@gmail.com>
2022-11-10 10:28:21 +0530

    pull

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Footer from '../Layout/Component/Footer'
-import NavBar from '../Layout/Component/NavBar'
+import React from "react";
+import Footer from "../Layout/Component/Footer";
+import NavBar from "../Layout/Component/NavBar";
 
 function Contact() {
   return (
     <>
-        <NavBar/>
-        <h1>Contact us</h1>
-        <Footer/>
+      <NavBar />
+      <h1>Contact us</h1>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
