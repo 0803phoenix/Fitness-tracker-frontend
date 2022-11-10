214f8b92e3a86dda7f1ea00f3dbd07492a8dd7a6
0803phoenix <114726547+0803phoenix@users.noreply.github.com>
2022-11-10 12:35:19 +0530

    Update HomeCards.js

--- src/Layout/Component/HomeCards.js
+++ src/Layout/Component/HomeCards.js
@@ -1,26 +1,19 @@
 
 const HomeCards = () => {
   return (
-   <> 
+    <>
     <div className="card-group px-1 py-1" >
      <div className="card bg-dark text-white">
-      <img className="card-image" src="https://source.unsplash.com/1400x500/?cardio" alt="Exercise" />
+      <img className="card-image image-fluid" src="https://source.unsplash.com/1400x500/?cardio" alt="Exercise" />
       <div className="card-image-overlay">
       </div>
-
-      <div className="card-group px-2 pb-2">
-        <div className="card" >
-          <div className="card-body">
-            <h5 className="display-4">Special title treatment</h5>
-            <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <a href="" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div className="card bg-dark text-white">
-          <img className="card-image" src="https://source.unsplash.com/1400x500/?fitness" alt="Exercise" />
-          <div className="card-image-overlay">
-        </div>
+     </div>
+      <div className="card">
+       <div className="card-body">
+        <h5 className="display-4">Special title treatment</h5>
+        <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <a href="" className="btn btn-primary">Go somewhere</a>
       </div>
      </div>  
     </div>
@@ -28,33 +21,33 @@ const HomeCards = () => {
     <div className="card" >
        <div className="card-body">
         <h5 className="display-4">Special title treatment</h5>
-        <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <a href="" className="btn btn-primary">Go somewhere</a>
      </div>
-    </div>   
+    </div>  
      <div className="card bg-dark text-white">
-      <img className="card-image" src="https://source.unsplash.com/1400x500/?fitness" alt="Exercise" />
+      <img className="card-image image-fluid" src="https://source.unsplash.com/1400x500/?fitness" alt="Exercise" />
       <div className="card-image-overlay">
       </div>
      </div>
     </div>
     <div className="card-group px-1 pb-1" >
      <div className="card bg-dark text-white">
-      <img className="card-image" src="https://source.unsplash.com/1400x500/?gym" alt="Exercise" />
+      <img className="card-image image-fluid" src="https://source.unsplash.com/1400x500/?gym" alt="Exercise" />
       <div className="card-image-overlay">
       </div>
      </div>
       <div className="card">
        <div className="card-body">
         <h5 className="display-4">Special title treatment</h5>
-        <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        <p className="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <a href="" className="btn btn-primary">Go somewhere</a>
       </div>
      </div>  
     </div>  
-   </>  
+   </>
   );
 }
 
