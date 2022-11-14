8b6859acb0c0f7a6d0620e003d687c129429d6ae
0803phoenix <114726547+0803phoenix@users.noreply.github.com>
2022-11-15 01:14:59 +0530

    Update Diet.js

--- src/Pages/Diet.js
+++ src/Pages/Diet.js
@@ -16,30 +16,7 @@ function Diet() {
         title="Keep Track of Your Diets"
         text=""
       />
-      <DietCards />
-
-      <div className="card bg-dark text-white m-1"
-        onClick={() => navigate("/reccommendation")}>
-        <img className="card-img" src="https://source.unsplash.com/1400x300/?menu" style={{ filter: "brightness(50%)" }} alt="Card image" />
-        <div className="card-img-overlay" style={
-          {
-            position: "absolute",
-            top: "20%",
-            left: "0",
-            width: "100%",
-          }
-        }>
-          <h5 className="card-title" style={{ fontSize: "40px", fontWeight: "light" }}>Get Diet reccommendations!</h5>
-          <p className="lead text-justify p-3 ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </div>
-
-      {/* <div
+          <div
         onClick={() => navigate("/reccommendation")}
         className="card p-5">
         <h1 className="display-4">Get Diet Recommendations!</h1>
@@ -50,8 +27,8 @@ function Diet() {
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </div>
-      </div> */}
-
+      </div>
+      <DietCards />
       <Footer />
     </>
   );
