5bbdca114c592c02bbae981d05a17e4137fd3cf0
architdas00001 <66167957+architdas00001@users.noreply.github.com>
2022-11-14 20:37:05 +0530

    Latest reg n cust

--- src/Authentication/Component/Register.js
+++ src/Authentication/Component/Register.js
@@ -4,10 +4,16 @@ import userDto from "./../Model/UserDto";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import UserService from './../Service/UserService';
+import { useForm } from "react-hook-form";
 
 function Register() {
-  const [user, setUser] = useState({ register: new userDto() });
-
+  const {register, formState:{errors}, handleSubmit}=useForm();
+  console.log(errors);
+  let b=errors;
+  const [user, setUser] = useState({ reg: new userDto() });
+  const onSubmit=(data) =>{
+    console.log(data);
+  }
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -24,59 +30,84 @@ function Register() {
         </Modal.Header>
 
         <Modal.Body>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label>Username</label>
               <input
                 className="form-control"
                 type="text"
                 id="username"
+                name="username"
+                {...register("username",{required: true})}
                 placeholder="Enter User Name"
-                value={user.register.usename}
+                value={user.reg.usename}
+                
                 onChange={(e) =>
-                  setUser({ register: { ...user.register, username: e.target.value } })
+                  setUser({ reg: { ...user.reg, username: e.target.value } })
                 }
               />
             </div>
+            <p style={{color:"red"}}>{errors.username?.type==="required" && "Username is required"}</p>
             <div className="form-group">
               <label>Password</label>
               <input
                 className="form-control"
                 type="password"
+                name="password"
                 id="password"
                 placeholder="Enter Password"
-                value={user.register.password}
+                {...register("password",{required: true,
+                minLength:8,
+                maxLength:14,
+                pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,})}
+                value={user.reg.password}
                 onChange={(e) =>
-                  setUser({ register: { ...user.register, password: e.target.value } })
+                  setUser({ reg: { ...user.reg, password: e.target.value } })
                 }
               />
             </div>
-          </form>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="success" onClick={() => navigate("/login")}>
-            Already a Member
-          </Button>
-          <Button variant="secondary"
-            onClick={
-              (e) => {
-                e.preventDefault();
-                // alert(JSON.stringify(user.register));
-                service
-                  .register(user.register)
-                  .then((result) => {
-                    // console.log("hi")
-                    sessionStorage.setItem('currentUser', JSON.stringify(result));
-                    navigate("/login");
-                  })
-                  .catch((error) => {
-                    alert(error.response.message);
-                  })
-              }}>
+            <p style={{color:"red"}}>
+            {errors.password?.type==="minLength" && "Length should be between 8 and 14"}
+            {errors.password?.type==="required" && "Password is required"}
+            
+            {errors.password?.type==="maxLength" && "Length should be between 8 and 14"}
+            {errors.password?.type==="pattern" && "It should have at least one number, one symbol, one uppercase and one lower case character"}</p>
+            
+            <Modal.Footer>
+                <Button variant="success" onClick={() => navigate("/login")}>
+                   Already a Member
+                </Button>
+                <Button variant="secondary" type="submit"
+                onClick={
+                  (e) => {
+                    
+                   console.log(errors);
+                   
+               //e.preventDefault();
+               // alert(JSON.stringify(user.register));
+               if((b===errors))
+               {
+                console.log("--------");
+                console.log(b);
+                 service
+                 .registers(user.reg)
+                 .then((result) => {
+                   // console.log("hi")
+                   sessionStorage.setItem('currentUser', JSON.stringify(result));
+                   navigate("/login");
+                 })
+                 .catch((error) => {
+                   alert(error.response.message);
+                 })
+               }
+             }}>
             Register
           </Button>
         </Modal.Footer>
+          </form>
+        </Modal.Body>
+
+       
       </Modal.Dialog>
     </>
   );
